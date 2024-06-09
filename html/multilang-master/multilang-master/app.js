ca5a714789b57def4dbd0cdaebd055087d583448
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-06-09 04:31:37 +0300

    Update app.js

--- html/multilang-master/multilang-master/app.js
+++ html/multilang-master/multilang-master/app.js
@@ -1,6 +1,6 @@
 const maxImg = document.querySelector('.right-panel img');
 const select = document.querySelector('select');
-const allLang = ['en', 'ru', 'ua'];
+const allLang = ['ua' 'en', 'ru', 'ае'];
 
 document.querySelectorAll('.left-panel img').forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
 
