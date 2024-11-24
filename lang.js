3e3ef521b4cf117996b176e4564400a1588d3886
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-11-24 11:43:09 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,6 +1,6 @@
 const langArr = {
  "js-head": {
    "uk": "<b>Furiya AI</b><span style=font-weight: 400> - проект присвячений мистецтву інтелекту для генерації картинок антропоморфних тварин або простіше "AI Images Furry.</span>",
-   "en": "<b>Furiya AI</b><span style=font-weight: 400> - проект присвячений мистецтву інтелекту для генерації картинок антропоморфних тварин або простіше "AI Images Furry.</span>",    
+   "en": "<b>Furiya AI</b><span style=font-weight: 400> - 1111111.</span>",    
     }, 
 }
